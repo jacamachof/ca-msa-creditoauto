36a5e17586a0b5ac6dcda98f5a7e468db9272aa8
Joaquin Camacho <ssrmatu@gmail.com>
2023-05-12 19:31:01 -0400

    Fixed bug

--- src/main/java/com/pichincha/ca/creditoauto/application/service/CarServiceImpl.java
+++ src/main/java/com/pichincha/ca/creditoauto/application/service/CarServiceImpl.java
@@ -75,7 +75,13 @@ public class CarServiceImpl implements CarService {
   public void deleteById(Long id) {
     var car = carRepository.findById(id);
     validateCreditRequests(car);
-    creditRequestRepository.deleteById(id);
+
+    if (car.getCreditRequests() != null && !car.getCreditRequests().isEmpty()) {
+      for (var creditRequest : car.getCreditRequests()) {
+        creditRequestRepository.deleteById(creditRequest.getId());
+      }
+    }
+
     carRepository.deleteById(id);
   }
 
